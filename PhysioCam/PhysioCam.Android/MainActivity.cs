91cef26a52f2a03440da9f049411f2e3237b6e05
Mathias Hammer <rabbyraptor@hotmail.com>
2021-04-19 16:36:07 +0200

    Changed layout direction to RTL

--- PhysioCam/PhysioCam.Android/MainActivity.cs
+++ PhysioCam/PhysioCam.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android.Views;
 
 namespace PhysioCam.Droid
 {
@@ -12,10 +13,13 @@ namespace PhysioCam.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            ToolbarResource = Resource.Layout.Toolbar;
             base.OnCreate(savedInstanceState);
-
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            Window.DecorView.LayoutDirection = LayoutDirection.Rtl;
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
